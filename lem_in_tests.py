042eb2dcf46d58a6528b5052d6c0b4dcb3957c38
Jeyne arryn Ziemann <jziemann@at-h4.21-school.ru>
2019-08-07 19:08:45 +0300

    parse graph generator

--- lem_in_tests.py
+++ lem_in_tests.py
@@ -6,25 +6,62 @@ flag = sys.argv[1]
 test_file = 'konstantikk_test_file.txt'
 
 os.system('./generator.dms %s > %s' % (flag, test_file))
-graph = dict()
+# graph = dict()
+start_flag = False
+end_flag = False
 with open(test_file, 'r') as f:
     inp = f.read().split('\n')
-    for x in inp:
-        if ' ' in x:
-            x = x.partition(' ')
-            graph[x[0]] = None
-         #   print(x)
-        elif '-' in x:
-            x = x.partition('-')
-            #graph['x[0]'] += [x[2]]
-            #graph['x[2]'] += [x[0]]
-            print (x[0], x[2])
-        elif "##start" in x:
-            x = f.readline()
-            x.partition(' ')
-            #print("Start :".x)
-        elif "##end" in x:
-            x = f.readline()
-            x.partition(' ')
-            #print("End :".x)
-print(graph)
+
+ants_count, steps_count, graph = int(inp[0]), int(inp[1].partition(': ')[2]), [i for i in inp[2:] if i.count('#') != 1 and i != ""]
+nodes = [line.split(' ')[0] for line in graph if '-' not in line]
+start = nodes[nodes.index('##start') + 1]
+end = nodes[nodes.index('##end') + 1]
+nodes.remove('##start')
+nodes.remove('##end')
+nodes = {i: [] for i in nodes}
+for line in graph[len(nodes) + 2:]:
+    part_line = line.partition('-')
+    nodes[part_line[0]].append(part_line[2])
+    nodes[part_line[2]].append(part_line[0])
+print(nodes)
+
+#     for x in inp:
+#         if x.isnumeric():
+#
+#         if '#Here' in x:
+#             print (x)
+#             continue
+#         if "##start" in x:
+#             start_flag = True
+#             continue
+#         elif "##end" in x:
+#             end_flag = True
+#             continue
+#         elif ' ' in x:
+#             if start_flag:
+#                 x = x.partition(' ')
+#                 graph.setdefault(x[0], [])
+#                 start = x[0]
+#                 start_flag = False
+#                 continue
+#             elif end_flag:
+#                 x = x.partition(' ')
+#                 graph.setdefault(x[0], [])
+#                 end = x[0]
+#                 end_flag =  False
+#                 continue
+#             x = x.partition(' ')
+#             graph.setdefault(x[0], [])#graph[x[0]] = list()
+#          #  print(x)
+#         elif '-' in x:
+#             x = x.partition('-')
+#             graph[x[0]].append(x[2])
+#             graph[x[2]].append(x[0])
+# print(graph)
+# print (start, end)
+# test2_file = "tests"
+# os.system('./lem-in < %s > %s'% (test_file, test2_file))
+# with open(test2_file, 'r') as f:
+#     inp = f.read().split('\n')
+#     for x in inp:
+
