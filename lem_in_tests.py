092d5af8cb202a59c0c1b30c900a9342f9d0b2cd
Vera <verakonst_tt@list.ru>
2019-08-09 23:17:02 +0300

    test for lem-in

--- lem_in_tests.py
+++ lem_in_tests.py
@@ -1,14 +1,16 @@
 import sys
 import os
+from time import time
 
+
+valid = True
 flag = sys.argv[1]
-
-test_file = 'konstantikk_test_file.txt'
-
-os.system('./generator.dms %s > %s' % (flag, test_file))
-# graph = dict()
-start_flag = False
-end_flag = False
+test = 'lem_in_output.txt'
+test_file = 'lem_in_input.txt'
+os.system('./generator.dms %s > %s ' % (flag, test_file))
+timer = time()
+os.system('./lem-in < %s > %s' % (test_file, test))
+timer = time() - timer
 with open(test_file, 'r') as f:
     inp = f.read().split('\n')
 
@@ -19,49 +21,55 @@ end = nodes[nodes.index('##end') + 1]
 nodes.remove('##start')
 nodes.remove('##end')
 nodes = {i: [] for i in nodes}
+
 for line in graph[len(nodes) + 2:]:
     part_line = line.partition('-')
     nodes[part_line[0]].append(part_line[2])
     nodes[part_line[2]].append(part_line[0])
-print(nodes)
 
-#     for x in inp:
-#         if x.isnumeric():
-#
-#         if '#Here' in x:
-#             print (x)
-#             continue
-#         if "##start" in x:
-#             start_flag = True
-#             continue
-#         elif "##end" in x:
-#             end_flag = True
-#             continue
-#         elif ' ' in x:
-#             if start_flag:
-#                 x = x.partition(' ')
-#                 graph.setdefault(x[0], [])
-#                 start = x[0]
-#                 start_flag = False
-#                 continue
-#             elif end_flag:
-#                 x = x.partition(' ')
-#                 graph.setdefault(x[0], [])
-#                 end = x[0]
-#                 end_flag =  False
-#                 continue
-#             x = x.partition(' ')
-#             graph.setdefault(x[0], [])#graph[x[0]] = list()
-#          #  print(x)
-#         elif '-' in x:
-#             x = x.partition('-')
-#             graph[x[0]].append(x[2])
-#             graph[x[2]].append(x[0])
-# print(graph)
-# print (start, end)
-# test2_file = "tests"
-# os.system('./lem-in < %s > %s'% (test_file, test2_file))
-# with open(test2_file, 'r') as f:
-#     inp = f.read().split('\n')
-#     for x in inp:
+ants_in_node = nodes.fromkeys(nodes.keys(), 0)
+ants_in_node[start] = ants_count
+ants = dict()
+
+with open(test, 'r') as f:
+    inp = f.read().split('\n')
+
+if inp[-1]:
+    valid = False
+    print('\033[91m%s\033[0m' % "Error. no \\n at the end of output.")
+else:
+    inp = inp[:-1]
+
+for index_line, line in enumerate(inp):
+    for index_step, step in enumerate(line.split()):
+        ant, next_node = step.partition('-')[::2]
+        if ant not in ants:
+            ants[ant] = start
+        ants_in_node[ants[ant]] -= 1
+        if next_node in nodes[ants[ant]]:
+            ants[ant] = next_node
+            ants_in_node[next_node] += 1
+        else:
+            print("In line: %d step: %d\n\033[91m%s\033[0m " %
+                  (index_line, index_step, 'Error. Incorrect link'))
+            sys.exit(' '.join(['\033[93m%s\033[0m' % s if i == index_step else s
+                               for i, s in enumerate(line.split())]))
+        if ants_in_node[next_node] != 1 and next_node != end:
+            print("In line: %d step: %d\n\033[91m%s\033[0m " %
+                  (index_line, index_step, 'Error. Node overflow'))
+            sys.exit(' '.join(['\033[93m%s\033[0m' % s if i == index_step else s
+                               for i, s in enumerate(line.split())]))
+if ants_in_node[start] != 0:
+    sys.exit('\033[91m%s\033[0m' % 'Error. %d ants in start in the end of the game' % ants_in_node[start])
+
+if ants_in_node[end] != ants_count:
+    sys.exit('\033[91m%s\033[0m' % "Error. %d ants did't reach the finish" % (ants_count - ants_in_node[end]))
+
+if valid:
+    print("\033[92m%s\033[0m" % "Valid output")
+print("time:", ("\033[92m%s\033[0m" if timer < 3 else "\033[91m%s\033[0m") %
+      timer)
+print("steps:", ("\033[92m%s(%s)\033[0m" if steps_count - len(inp) <= 0 else "\033[91m%s(%s)\033[0m") %
+      (len(inp), steps_count))
+
 
