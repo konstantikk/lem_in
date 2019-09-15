efe3896bc1397fd961604ca531cb14fadf0ee39c
Kochetkov Nickolai <vlegros@21-SCHOOL.RU>
2019-09-15 18:24:58 +0300

    header prototypes changes

--- lem_in.h
+++ lem_in.h
@@ -48,18 +48,18 @@ typedef struct	s_path
 typedef struct  s_room
 {
     char    *name;
+    t_node	*ptr;
     int     temp_ant;
 }               t_room;
 
 typedef struct	s_node
 {
+	char	*print_line;
 	char	*name;
 	t_pvec	*links;
 	int	level;
 	int potential;
 	struct s_node *parent;
-	int x;
-	int y;
 	int used:1;
 }				t_node;
 
@@ -96,7 +96,7 @@ t_vec    *get_flow(t_farm *farm);
 int 	*check_profit(t_farm *farm, t_vec *flow, int max);
 void    let_the_flow_go(t_farm **farm_ptr, t_flow **flow, int *ants_allocation);
 int     ht_insert_node(t_ht *hashtable, t_node *node);
-t_node	*create_node(char *name);
+t_node	*create_node(char *name, char *buff);
 int     ht_enlarge(t_ht *hashtable);
 t_node  *ht_find_node(t_ht *hashtable, char *name);
 int		read_start_end(t_farm **farm, int fd, char **buff, int start_end);
@@ -109,7 +109,7 @@ t_link	*safe_create_link(t_farm **farm, char *name);
 void    safe_pushback(t_farm **farm, t_pvec *links, void *elem);
 void    safe_insert(t_farm **farm, t_ht *nodes, t_node *node);
 t_path    *create_path(void);
-t_room  *create_room(char *name);
+t_room  *create_room(char *name, t_node *node_ptr);
 void    del_path(void **elem);
 void    safe_room_adding(t_farm **farm_ptr, t_path **path_ptr, void *elem, t_pvec **flow);
 void	ft_recover_path(t_farm **farm_ptr, t_link *link, t_pvec **flow);
@@ -121,5 +121,6 @@ void        dijkstra(t_farm *farm);
 void	ft_add_path(t_farm **farm_ptr);
 void    recalculate_potentials(t_ht *nodes);
 int     new_alg(t_farm **farm_ptr);
+int				ft_release_flow(t_farm **farm_ptr);
 
 #endif
